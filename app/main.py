08d01974206d57c85ccc6f0d9f0635520bc9110e
Icosa <117495728+H4drian@users.noreply.github.com>
2024-08-31 05:05:27 -0400

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,32 +1,27 @@
 from pyurbandict import UrbanDict
-
 import dictdisplay
 import parser
 
-def main():
-    args = parser.parse_args()
+args = parser.parse_args()
 
-    word = args.word
-    entry = args.entry
-    extra_entries = args.extra_entries
+word = args.word
+entry = args.entry
+extra_entries = args.extra_entries
 
-    total_entries = len(UrbanDict(word).search())
+total_entries = len(UrbanDict(word).search())
 
-    if entry > total_entries:
-        print(f"Urban: Error: entry {entry} does not exist for word \"{word}\"")
-        return
+if entry > total_entries:
+    print(f"Urban: Error: entry {entry} does not exist for word \"{word}\"")
+    return
 
-    if extra_entries > total_entries:
-        extra_entries = total_entries - 1
+if extra_entries > total_entries:
+    extra_entries = total_entries - 1
 
-    while True:
-        dictdisplay.print_entry(word, entry)
+while True:
+    dictdisplay.print_entry(word, entry)
 
-        extra_entries -= 1
-        entry += 1
+    extra_entries -= 1
+    entry += 1
 
-        if extra_entries <= 0:
-            break
-
-if __name__ == "__main__":
-    main()
+    if extra_entries <= 0:
+        break
